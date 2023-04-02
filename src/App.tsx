dd8912175d288e6271b2ed6cf96cf9616cebba49
Antonio <antonie.dev@gmail.com>
2023-04-02 19:50:54 -0300

    finished app

--- src/App.tsx
+++ src/App.tsx
@@ -48,41 +48,41 @@ export function App() {
   })();
 
   return (
-    <div className="p-8">
+    <div className="p-8 bg-gray-900 text-gray-100">
       <div className="flex space-x-4 mb-4">
         <select
-          className="custom-input bg-gray-50 border border-gray-300 px-3 py-2 rounded-md text-sm font-medium text-gray-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
+          className="custom-input bg-gray-800 border border-gray-600 px-3 py-2 rounded-md text-sm font-medium text-gray-100 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
           name="filter"
           value={filterType}
           onChange={(e) => setFilterType(e.target.value as "name" | "date")}
         >
-          <option value="name">Filtrar por nome</option>
-          <option value="date">Filtrar por data</option>
+          <option value="name">Name</option>
+          <option value="date">Last update</option>
         </select>
       </div>
       {filterType === "name" && (
         <input
-          className="custom-input bg-gray-50 border border-gray-300 px-3 py-2 rounded-md text-sm font-medium text-gray-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
+          className="custom-input bg-gray-800 border border-gray-600 px-3 py-2 rounded-md text-sm font-medium text-gray-100 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
           type="text"
           name="search"
-          placeholder="Buscar..."
+          placeholder="Find a repository..."
           onChange={(e) => setSearch(e.target.value)}
           value={search}
         />
       )}
       {filterType === "date" && (
-        <p className="text-sm font-medium text-gray-500 mb-2">
+        <p className="text-sm font-medium text-gray-400 mb-2">
           Repositórios ordenados pela última atualização
         </p>
       )}
       <div className="flex space-x-4 mb-4">
         <select
-          className="custom-input bg-gray-50 border border-gray-300 px-3 py-2 rounded-md text-sm font-medium text-gray-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
+          className="custom-input bg-gray-800 border border-gray-600 px-3 py-2 rounded-md text-sm font-medium text-gray-100 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
           name="language"
           value={selectedLanguage}
           onChange={(e) => setSelectedLanguage(e.target.value)}
         >
-          <option value="">Todas as linguagens</option>
+          <option value="">All languages</option>
           {languages.map((lang) => (
             <option value={lang} key={lang}>
               {lang}
@@ -90,22 +90,28 @@ export function App() {
           ))}
         </select>
       </div>
-      <div className="bg-white rounded-lg shadow w-96 p-4">
-        <ul className="divide-y divide-gray-200 p-2">
+      <div className="bg-gray-800 rounded-lg shadow w-96 p-4">
+        <ul className="divide-y divide-gray-700 p-2">
           {filteredRepos.map((repo) => (
-            <li className="py-1" key={repo.name}>
+            <li
+              className="py-1 hover:bg-blue-600 transform transition duration-300 hover:scale-105"
+              key={repo.name}
+            >
               <div className="flex space-x-3">
-                <h3 className="text-sm font-medium text-blue-500">
+                <h3 className="text-sm font-medium text-blue-400">
                   {repo.name}
                 </h3>
+                <span className="inline-block bg-yellow-400 rounded-full px-2 py-1 text-xs font-semibold text-gray-800">
+                  {repo.stargazers_count} Stars
+                </span>
               </div>
-              <p className="text-sm text-gray-500">
+              <p className="text-sm text-gray-400">
                 Descrição: {repo.description}
               </p>
-              <p className="text-sm font-normal text-orange-500">
+              <p className="text-sm font-normal text-yellow-400">
                 {repo.language}
               </p>
-              <p className="text-sm font-normal text-gray-500">
+              <p className="text-sm font-normal text-gray-400">
                 Última atualização:{" "}
                 {new Date(repo.updated_at).toLocaleDateString()}
               </p>
